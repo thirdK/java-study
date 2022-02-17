340f32de4851b2ffed9a7acf9dc4b7d1796e0159
thirdK <dla0625@gmail.com>
2022-02-17 23:15:44 +0900

    Create LinkedList.java

---
+++ src/review_dataStructure_algorithm/LinkedList.java
@@ -0,0 +1,40 @@
+package review_dataStructure_algorithm;
+
+public class LinkedList {
+	Node header;
+
+	public LinkedList() {
+		header = new Node();
+	}
+
+	void append(int d) {
+		Node end = new Node();
+		end.data = d;
+		Node n = header;
+		while (n.next != null) {
+			n = n.next;
+		}
+		n.next = end;
+	}
+
+	void delete(int d) {
+		Node n = header;
+		while (n.next != null) {
+			if (n.next.data == d) {
+				n.next = n.next.next;
+			} else {
+				n = n.next;
+			}
+		}
+	}
+
+	void retrieve() {
+		Node n = header.next; 
+
+		while (n.next != null) {
+			System.out.print(n.data + " -> ");
+			n = n.next;
+		}
+		System.out.println(n.data);
+	}
+}
