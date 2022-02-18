1069507b7319add504d1d40e5c60ec38bb7b5579
thirdK <dla0625@gmail.com>
2022-02-19 00:04:35 +0900

    Update LinkedList.java

--- src/review_dataStructure_algorithm/LinkedList.java
+++ src/review_dataStructure_algorithm/LinkedList.java
@@ -1,21 +1,68 @@
 package review_dataStructure_algorithm;
 
-
 class Node {
 	int data;
 	Node next = null;
-	
+
 	public Node() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public Node(int data) {
 		this.data = data;
 		next = null;
 	}
+	
+	Node get(int d) {
+		Node n = this;
+		for(int i=0; i<d; i++) {
+			n=n.next;
+		}
+		return n;
+	}
+
+//	Node get(int d) { //d번째 노드를 반환받는 메소드
+//		Node n = this;
+//		int cnt=0;
+//		while(n != null) {
+//			if(cnt == d) return n;
+//			cnt++;
+//			n = n.next;
+//		}
+//		return null;
+//	}
+
+	Node addNext(int d) {//다음 노드로 추가
+		Node n = this;
+		Node next = new Node(d);
+
+		n.next = next;
+	
+		return next;
+	}
+
+	Node addNext(Node node) {//다음 노드로 추가
+		Node n = this;
+		Node next = node;
+		
+		n.next = next;
+		
+		return next;
+	}
+	
+	void print() {//출력
+		Node n = this;
+		while (n.next != null) {
+			System.out.print(n.data + " -> ");
+			n = n.next;
+		}
+		System.out.print(n.data);
+		System.out.println();
+	}
 }
 
 public class LinkedList {
+
 	Node header;
 
 	public LinkedList() {
@@ -44,7 +91,7 @@ public class LinkedList {
 	}
 
 	void retrieve() {
-		Node n = header.next; 
+		Node n = header.next;
 
 		while (n.next != null) {
 			System.out.print(n.data + " -> ");
