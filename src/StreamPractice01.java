8c8bdf8caee61d3335807e2cb3fdd16b070bb146
thirdK <dla0625@gmail.com>
2022-02-24 21:47:54 +0900

    Update StreamPractice01.java

--- src/StreamPractice01.java
+++ src/StreamPractice01.java
@@ -8,12 +8,23 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamPractice01 {
 	public static void main(String[] args) {
 		int[] arr = {1,1,10,2,5,30,4,2};	//stream 없이 배열의 중복 값을 없에고 내림차순으로 정렬해보자
 		List<Integer> list = new ArrayList<Integer>();
 		Set<Integer> set = new HashSet<Integer>();
+		
+		for(int i = 0; i<arr.length; i++) {
+			set.add(arr[i]);
+		}
+		Iterator<Integer> iter = set.iterator();
+		
+		for(int i=0; iter.hasNext(); i++) {
+			list.add(iter.next());
+		}
 		
 		list.sort(Comparator.reverseOrder());
 		
@@ -37,8 +48,28 @@ public class StreamPractice01 {
 //		Arrays.stream(배열명).가공메소드...
 //		리스트명.stream.가공메소드...
 //		이런 식으로 바로 사용해도 된다
+		System.out.println("==================================================================");
+		System.out.println("count() 배열, 컬렉션 크기 확인");
+		System.out.println(Arrays.stream(arr).count());
+		System.out.println(list.stream().count());
+		System.out.println();
+		System.out.println("sorted() 정렬");
+		System.out.println(Arrays.stream(arr).boxed().sorted().collect(Collectors.toList()));
+		System.out.println(list.stream().sorted().collect(Collectors.toList()));
+		System.out.println();
+		System.out.println("sorted(Comparator.reverseOrder() 역정렬");
+		System.out.println(Arrays.stream(arr).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList()));
+		System.out.println(list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList()));
 		
 		
+		System.out.println();
+		System.out.println("==================================================================");
+		//Stream 연습
+		System.out.print("스트림 연습 >> ");
+		IntStream.range(1,11).filter(i->i%2 == 0).forEach(System.out::println);
+				
+		//500이상의 숫자중 짝수이면서 5의 배수인 수의 합
+		System.out.println("스트림 연습 >> "+IntStream.range(0, 1001).skip(500).filter(i -> i%2==0).filter(i -> i%5==0).sum());		
 		
 	}
 }
