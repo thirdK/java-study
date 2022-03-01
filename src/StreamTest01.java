1a1674cfed0d2cab9905a4335b61cdce6f90157e
thirdK <dla0625@gmail.com>
2022-03-01 18:58:35 +0900

    Update StreamTest01.java

--- src/StreamTest01.java
+++ src/StreamTest01.java
@@ -15,7 +15,14 @@ public class StreamTest01 {
 	public static void main(String[] args) {
 		int[] data = {5, 6, 4, 2, 3, 1, 1, 2, 2, 4, 8};
 		
-		int[] result = Arrays.stream(data).boxed().filter(i -> i%2==0).distinct().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
+		int[] result = Arrays.stream(data)	//IntStream을 생성한다.
+				.boxed()	//IntStream을 Stream<Integer>로 변경한다. 
+				//(Comparator.reverseOrder와 같은 메소드는 원시타입인 int 대신 Integer를 사용해야한다.)
+				.filter(i -> i%2==0)	//짝수만 걸러낸다.
+				.distinct()	//중복을 제거한다.
+				.sorted(Comparator.reverseOrder())	//역순으로 정렬한다.
+				.mapToInt(Integer::intValue)	//Stream<Integer>를 IntStream으로 변경한다.
+				.toArray();	//int[] 배열로 반환한다.
 		
 		for(int i:result) {
 			System.out.println(i);
